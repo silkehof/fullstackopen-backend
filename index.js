186dc83a54ccc9520f0d89b6fd97e4001c8480fc
silkehof <silke@silkehofmann.eu>
2020-12-28 22:39:16 +0100

    Add solutions for exercises 3_1 to 3_2

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {   
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
+    }
+]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const timeElapsed = Date.now()
+const today = new Date(timeElapsed)
+
+app.get('/info', (request, response) => {
+    var responseText = 'The phonebook currently contains ' + persons.length + ' entries.<br>'
+    responseText += '<br>The request has been made on ' + today + '.'
+    response.send(responseText)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
