f84ff7a78dfdafd084e7e0c85359db5736f1fa1a
silkehof <silke@silkehofmann.eu>
2020-12-29 19:02:10 +0100

    Add solution for exercise 3_7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
@@ -25,6 +26,8 @@ let persons = [
         number: "39-23-6423122"
     }
 ]
+
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
