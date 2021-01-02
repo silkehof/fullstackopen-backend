e03d33eb1d4f3d879f687615c2ce4ab43423c644
silkehof <silke@silkehofmann.eu>
2021-01-02 01:43:03 +0100

    Add solution for exercise 3_9

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 let persons = [
     {   
